9e9f90126a92572605e53dae8e7ab9e84c9a24da
Jan <j.f.akerman@gmail.com>
2019-03-09 16:50:21 +0000

    Adds initial order API

--- handler.js
+++ handler.js
@@ -1,5 +1,70 @@
-'use strict';
+'use strict'
+const AWS = require('aws-sdk');
 
-module.exports.hello = async (event, context) => {
-  return "hi"
-};
+const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+const inputError = () => ({ statusCode: 400, body: JSON.stringify({ statusCode: 400, reason: 'Invalid input parameters'})})
+const success = () => ({ statusCode: 200 })
+
+const validateMakeOrder = input =>
+    input.username &&
+    input.breakfastId && Number.isInteger(input.breakfastId) &&
+    input.itemId && Number.isInteger(input.itemId)
+const validateDeleteOrder = input =>
+    input.username &&
+    input.breakfastId && Number.isInteger(input.breakfastId)
+
+module.exports.makeOrder = async (event, context) => {
+    console.log(event)
+
+    const body = event.body ? JSON.parse(event.body) : null
+    console.log(`Body: ${JSON.stringify(body)}`)
+    if (!body || !validateMakeOrder(body)) return inputError()
+
+    const params = {
+        TableName : 'orders',
+        Item: {
+            username: {
+                S: body.username
+            },
+            breakfastId: {
+                N: body.breakfastId.toString()
+            },
+            itemId: {
+                N: body.itemId.toString()
+            }
+        }
+    }
+
+    await dynamodb.putItem(params).promise()
+
+    const response = success()
+    console.log(`Response: ${JSON.stringify(response)}`)
+    return response
+}
+
+module.exports.deleteOrder = async (event, context) => {
+    console.log(event)
+
+    const body = event.body ? JSON.parse(event.body) : null
+    console.log(`Body: ${JSON.stringify(body)}`)
+    if (!body || !validateDeleteOrder(body)) return inputError()
+
+    const params = {
+        TableName : 'orders',
+        Key: {
+            username: {
+                S: body.username
+            },
+            breakfastId: {
+                N: body.breakfastId.toString()
+            }
+        }
+    }
+
+    await dynamodb.deleteItem(params).promise()
+
+    const response = { statusCode: 200 }
+    console.log(`Response: ${JSON.stringify(response)}`)
+    return  response
+}
